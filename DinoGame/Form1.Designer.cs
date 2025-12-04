3a94b899dc19937b98e473601af9eb9e81a1bfcb
AndreiBcs <andreibichis1@gmail.com>
2025-12-04 19:47:12 +0200

    mai trebuie lucrat la clasa game care va apela constant clasa drawBoard ca sa actualizeze fiecare miscare a patratelelor

--- DinoGame/Form1.Designer.cs
+++ DinoGame/Form1.Designer.cs
@@ -35,43 +35,47 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // Btn_Exit
+            // 
+            Btn_Exit.Location = new System.Drawing.Point(913, 38);
+            Btn_Exit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Btn_Exit.Name = "Btn_Exit";
+            Btn_Exit.Size = new System.Drawing.Size(46, 48);
+            Btn_Exit.TabIndex = 1;
+            Btn_Exit.Text = "Exit";
+            Btn_Exit.UseVisualStyleBackColor = true;
+            Btn_Exit.Click += new System.EventHandler(this.Btn_Exit_Click);
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 38);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1201, 401);
+            this.pictureBox1.Size = new System.Drawing.Size(901, 326);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // Btn_Exit
-            // 
-            Btn_Exit.Location = new System.Drawing.Point(1219, 187);
-            Btn_Exit.Name = "Btn_Exit";
-            Btn_Exit.Size = new System.Drawing.Size(61, 47);
-            Btn_Exit.TabIndex = 1;
-            Btn_Exit.Text = "Btn_Exit";
-            Btn_Exit.UseVisualStyleBackColor = true;
-            Btn_Exit.Click += new System.EventHandler(this.Btn_Exit_Click);
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1219, 283);
-            this.button1.Name = "Play";
-            this.button1.Size = new System.Drawing.Size(51, 58);
+            this.button1.Location = new System.Drawing.Point(913, 317);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 47);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Play";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Play_Click);
             // 
             // FormDino
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1282, 473);
+            this.ClientSize = new System.Drawing.Size(962, 384);
             this.Controls.Add(this.button1);
             this.Controls.Add(Btn_Exit);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormDino";
             this.Text = "FormDino";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- DinoGame/Form1.cs
+++ DinoGame/Form1.cs
@@ -12,36 +12,39 @@ namespace DinoGame
 {
     public partial class FormDino : Form
     {
-        int DIMENSIUNE=20;
+        int DIMENSIUNE = 20;
         PictureBox pictureBox;
-        int [][] Board;
+        int[][] Board;
         Graphics mDesen;
-        SolidBrush [] pensula;
+        SolidBrush[] pensula;
+        int dinoX;
+        int dinoY;
+        int dinoJump = 2;//asta ma gandeam sa o folosim pentru saritura,
+                         //atunci cand apesi space PLAYER-ul sare cu 2 pozitii in sus ca sa evitam obstacolele
+        bool isJumping = false;
+        int ground=10;//linia solului
+        Random rand = new Random();//pentru obstacole
 
-       public enum PATRATE{
-            GOL=1,
-            PLAYER=2,
-            OBSTACOL =3,
+        public enum PATRATE
+        {
+            GOL = 0,
+            PLAYER = 1,
+            OBSTACOL = 2,
 
         }
         public FormDino()
         {
             InitializeComponent();
             mDesen = pictureBox1.CreateGraphics();
-          
+
             pensula = new SolidBrush[3];
             pensula[0] = new SolidBrush(Color.White);
-            pensula[1] = new SolidBrush(Color.Red);
-            pensula[2] = new SolidBrush(Color.Blue);
-
-
-
-
-
+            pensula[1] = new SolidBrush(Color.Blue);
+            pensula[2] = new SolidBrush(Color.Red);
+            this.KeyDown += pressSpace;
 
         }
-       
-       
+
         private void Btn_Exit_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -50,7 +53,6 @@ namespace DinoGame
         private void Play_Click(object sender, EventArgs e)
         {
 
-          
             Board = new int[60][];
             for (int i = 0; i < 60; i++)
             {
@@ -60,7 +62,11 @@ namespace DinoGame
                     Board[i][j] = (int)PATRATE.GOL;
                 }
             }
+            drawBoard();
+        }
 
+        void drawBoard()
+        {
             for (int i = 0; i < 60; i++)
             {
                 for (int j = 0; j < 20; j++)
@@ -68,6 +74,28 @@ namespace DinoGame
                     mDesen.FillRectangle(pensula[Board[i][j]], i * DIMENSIUNE + 1, j * DIMENSIUNE + 1, DIMENSIUNE - 1, DIMENSIUNE - 1);
                 }
             }
+
         }
+
+        private void Game(object sender, EventArgs e)
+        {
+            dinoX = 12;
+            dinoY = 10;
+            Board[dinoX][dinoY] = (int)PATRATE.PLAYER;
+            mDesen.FillRectangle(pensula[Board[dinoX][dinoY]], dinoX * DIMENSIUNE + 1, dinoY * DIMENSIUNE + 1, DIMENSIUNE - 1, DIMENSIUNE - 1);
+
+
+            drawBoard();
+        }
+        private void pressSpace(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !isJumping)
+            {
+                isJumping = true;
+                dinoJump = -2;
+            }
+        }
+
+
     }
 }
